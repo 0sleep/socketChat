fa82719582ff7852f16e61e35c3fa7fdb880f1a3
0sleep <dominik@powells.eu>
2021-11-05 00:22:09 +0100

    change port

--- websockets.py
+++ websockets.py
@@ -23,4 +23,4 @@ def value_changed(message):
     emit('update value', message, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0')
+    socketio.run(app, host='0.0.0.0', port=8123)
